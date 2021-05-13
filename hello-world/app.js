b6f621ed866927769d541865faf38ecf1852bc7b
yuria <yuria.soft0615@gmail.com>
2021-05-13 13:40:40 +0900

    Change path /hello to /post

--- hello-world/app.js
+++ hello-world/app.js
@@ -15,6 +15,8 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
+    const x = (2 < 3 )?  1 : 0; //IDの存在性を確認する。
+    const title_list=["ERROR_NOT_FOUND","AWS Lambda から、ReactのBlogに至るテストです。","Second Blog","third Blog"]
     try {
         // const ret = await axios(url);
         response = {
@@ -30,7 +32,7 @@ exports.lambdaHandler = async (event, context) => {
                 "update": 20210408,
                 "create": 20210408,
                 "tags": [1,4,5],
-                "title": "AWS Lambda から、ReactのBlogに至るテストです。",
+                "title": title_list[x],
                 "prevPost": null,
                 "nextPost": null,
                 "post":" $X_C = \\dfrac{1}{j \\omega C}$\n環境\nPython 3.8\nWindows10/MacOS Catalina\n\n---\n\n##PythonでもElectronみたいにWEBフロントでGUI実装をしたい。\nということを朝起きて思ったことはありませんか?\n\ntkinterもいいけど、私はHTMLでアプリケーションのレイアウトを構築したい!\nそんなあなたにEELというライブラリーがあります。\n\n### 作れるもの\n![スクリーンショット 2020-12-12 214757.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/163357/1792c30f-f58f-d974-c3ae-bdfec836f796.png)\n\n※こちらは作成途中の画面です。\n\nマークダウンと下にツイッターくらいの文字数を想定したミニメモがかけるようなアプリをつくりました。\n出力すると、指定したところに日付のディレクトリーを作成しその中にブログ記事のMDファイルが書かれると行った構成です。\n\n### EELの使い方\n\nライブラリーをインストールするコマンドは以下のようになってます。\n\n```\n$pip install eel\n```"
